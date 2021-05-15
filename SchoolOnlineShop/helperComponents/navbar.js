cf9aceae3747b3c0f2a8b1bd6f5f52ce3ff6d993
crs <ornaldo.hasa@fshnstudent.info>
2021-05-16 01:36:51 +0200

    m

--- SchoolOnlineShop/helperComponents/navbar.js
+++ SchoolOnlineShop/helperComponents/navbar.js
@@ -1,8 +1,9 @@
 const fillNav = (elementSelector, list) => {
+  const activeType=new URLSearchParams(windowUrl.search).get("type");
   $(elementSelector).append(
     `${list
       .map(({name}) => {
-        return `<a href="${addUrlParams(`${pageLocation}/shopItems/shopItems.html`,[`type=${name}`])}"><span>${name}</span></a>`;
+        return `<a class="${activeType === name ?"active" : ""}" href="${addUrlParams(`${pageLocation}/shopItems/shopItems.html`,[`type=${name}`])}"><span>${name}</span></a>`;
       })
       .join("")}`
   );
